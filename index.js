8d2c600527e72c055dc07ad5cbeccbd31853dff4
Neha <neha94dave@gmail.com>
2024-01-03 21:26:43 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
-
+//hello
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: "10mb" }))
